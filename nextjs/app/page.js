c55b58224ab19ccbd4f3b8ef3fa96cb104cf6531
KHWCoding <127857900+KHWCoding@users.noreply.github.com>
2024-10-03 20:55:25 +0900

    4th Commit
    Next.js - 생활코딩 공부했던 코딩내용 업로드

---
+++ nextjs/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h2>Welcome</h2>
+      Hello, WEB!
+      <br></br><img src="/헤일로 슬라임.png"></img>
+    </>
+  );
+}
